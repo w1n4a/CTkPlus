099a07f4965da429a3f5e5a275bd31a7f9d6612e
w1n4a <w1n4axd@gmail.com>
2026-09-20 22:46:02 +0300

    КАСТОМИЗАЦИЯ

--- ctkplus/CTkPlus.py
+++ ctkplus/CTkPlus.py
@@ -1,40 +1,40 @@
 import customtkinter as tk
 
 class CTkEntryButton(tk.CTkFrame):
-    def __init__(self, master, command=None, text='✔️', placeholder_text=None, bg_color="transparent", fg_color="transparent", **kwargs):
+    def __init__(self, master, command=None, text='', placeholder_text=None, placeholder_text_color=None, width=140, height=28, border_width=1, bg_color="transparent", fg_color="transparent", entry_fg_color=None, border_color=None, text_color=None, font=None, button_fg_color=None, button_hover_color=None, button_text_color=None, **kwargs):
         super().__init__(master, bg_color=bg_color,fg_color=fg_color, **kwargs)
 
-        self.entry = tk.CTkEntry(self, placeholder_text=placeholder_text)
+        self.entry = tk.CTkEntry(self, width=width, height=height, corner_radius=0, border_width=border_width, bg_color="transparent", fg_color=entry_fg_color, border_color=border_color, text_color=text_color, placeholder_text_color=placeholder_text_color, font=font, placeholder_text=placeholder_text)
         self.entry.pack(side='left')
 
-        self.button = tk.CTkButton(self,width=5,text=text,font=('Classic',15),command=command)
-        self.button.pack(padx=2,side='left')
+        self.button = tk.CTkButton(self, width=20, text=text, font=('Arial', 15), corner_radius=0, border_width=border_width, border_color=border_color, fg_color=button_fg_color, hover_color=button_hover_color, text_color=button_text_color, command=command)
+        self.button.pack(side='left', fill='y')
 
     def get(self):
         return self.entry.get()
 
 class CTkSpinBox(tk.CTkFrame):
-    def __init__(self, master,command=lambda *args: None,step=1,range=('inf','inf'),start_num=0,bg_color="transparent", fg_color="transparent", **kwargs):
-        super().__init__(master,bg_color=bg_color,fg_color=fg_color)
+    def __init__(self, master, command=lambda *args: None, step=1, range=('inf','inf'), start_num=0, width=140, height=28, border_width=1, bg_color="transparent", fg_color="transparent", entry_fg_color=None, border_color=None, text_color=None, font=None, button_fg_color=None, button_hover_color=None, button_text_color=None, **kwargs):
+        super().__init__(master, bg_color=bg_color, fg_color=fg_color, **kwargs)
 
         self.range = range
 
         self.string = tk.StringVar(value=str(start_num))
         self.string.trace_add('write',command)
 
-        self.entry = tk.CTkEntry(self,textvariable=self.string,corner_radius=0)
+        self.entry = tk.CTkEntry(self, width=width, height=height, corner_radius=0, border_width=border_width, bg_color="transparent", fg_color=entry_fg_color, border_color=border_color, text_color=text_color, font=font, textvariable=self.string)
         self.entry.pack(side='left')
 
         self.frame = tk.CTkFrame(self)
         self.frame.pack(side='left')
 
-        self.button = tk.CTkButton(self.frame,width=15,height=14,text='▲',font=('Impact',5,'bold'),corner_radius=0,command=lambda: self.plus(step))
+        self.button = tk.CTkButton(self.frame, width=15, height=self.entry._current_height/2, text='▲', font=('Impact', 5, 'bold'), bg_color='transparent', corner_radius=0, fg_color=button_fg_color, hover_color=button_hover_color, text_color=button_text_color, command=lambda: self.plus(step))
         self.button.pack(side='top',pady=0)
 
-        self.button2 = tk.CTkButton(self.frame,width=15,height=14,text='▼',font=('Impact',5,'bold'),corner_radius=0,command=lambda: self.minus(step))
+        self.button2 = tk.CTkButton(self.frame, width=15, height=self.entry._current_height/2, text='▼', font=('Impact', 5, 'bold'), bg_color='transparent', corner_radius=0, fg_color=button_fg_color, hover_color=button_hover_color, text_color=button_text_color, command=lambda: self.minus(step))
         self.button2.pack(side='bottom',pady=0)
 
-    def plus(self,*args):
+    def plus(self, *args: any):
         try:
             r = self.string.get()
             if self.range[1] == 'inf' or int(r) + sum(args) <= self.range[1]:
@@ -44,9 +44,9 @@ class CTkSpinBox(tk.CTkFrame):
                 self.string.set(self.range[1])
                 return self.string.get()
         except:
-            pass
+            raise TypeError
 
-    def minus(self, *args):
+    def minus(self, *args: any):
         try:
             r = self.string.get()
             if self.range[0] == 'inf' or int(r) - sum(args) >= self.range[0]:
@@ -56,7 +56,7 @@ class CTkSpinBox(tk.CTkFrame):
                 self.string.set(self.range[0])
                 return self.string.get()
         except:
-            pass
+            raise TypeError
 
     def get(self):
         return self.string.get()
@@ -66,10 +66,10 @@ class CTkSpinBox(tk.CTkFrame):
             self.string.set(str(pos))
             return self.string.get()
         else:
-            return TypeError()
+            raise TypeError()
 
 class CTkEntry(tk.CTkEntry):
-    def __init__(self, master, command_type = 'write', text='', width = 140, height = 28, corner_radius = None, border_width = None, bg_color = "transparent", fg_color = None, border_color = None, text_color = None, placeholder_text_color = None, placeholder_text = None, font = None, command=lambda *args: None, **kwargs):
+    def __init__(self, master, command_type = 'write', text='', width = 140, height = 28, corner_radius = None, border_width = 1, bg_color = "transparent", fg_color = None, border_color = None, text_color = None, placeholder_text_color = None, placeholder_text = None, font = None, command=lambda *args: None, **kwargs):
         super().__init__(master, width, height, corner_radius, border_width, bg_color, fg_color, border_color, text_color, placeholder_text_color, placeholder_text, font, **kwargs)
 
         self.string = tk.StringVar(value=text)
@@ -77,7 +77,7 @@ class CTkEntry(tk.CTkEntry):
         self.configure(textvariable=self.string)
 
 class CTkSelector(tk.CTkFrame):
-    def __init__(self, master,current=0,values=[],command=lambda *args: None,start_num=0,bg_color="transparent", fg_color="transparent", **kwargs):
+    def __init__(self, master, current=0, values=[], command=lambda *args: None, width=140, height=28, border_width=1, bg_color="transparent", fg_color="transparent", entry_fg_color=None, border_color=None, text_color=None, font=None, button_fg_color=None, button_hover_color=None, button_text_color=None, **kwargs):
         super().__init__(master,bg_color=bg_color,fg_color=fg_color)
 
         self.cur = current
@@ -86,19 +86,19 @@ class CTkSelector(tk.CTkFrame):
         self.string = tk.StringVar(value=self.val[self.cur])
         self.string.trace_add('write',command)
 
-        self.entry = tk.CTkEntry(self,textvariable=self.string,corner_radius=0,state='disabled')
+        self.entry = tk.CTkEntry(self, width=width, height=height, corner_radius=0, border_width=border_width, bg_color="transparent", fg_color=entry_fg_color, border_color=border_color, text_color=text_color, font=font, textvariable=self.string, state='disabled')
         self.entry.pack(side='left')
 
         self.frame = tk.CTkFrame(self)
         self.frame.pack(side='left')
 
-        self.button = tk.CTkButton(self.frame,width=15,height=14,text='▲',font=('Impact',5,'bold'),corner_radius=0,command=lambda: self.up(1))
+        self.button = tk.CTkButton(self.frame, width=15, height=self.entry._current_height/2, text='▲', font=('Impact', 5, 'bold'), corner_radius=0, fg_color=button_fg_color, hover_color=button_hover_color, text_color=button_text_color, command=lambda: self.up(1))
         self.button.pack(side='top',pady=0)
 
-        self.button2 = tk.CTkButton(self.frame,width=15,height=14,text='▼',font=('Impact',5,'bold'),corner_radius=0,command=lambda: self.down(1))
+        self.button2 = tk.CTkButton(self.frame, width=15, height=self.entry._current_height/2, text='▼', font=('Impact', 5, 'bold'), corner_radius=0, fg_color=button_fg_color, hover_color=button_hover_color, text_color=button_text_color, command=lambda: self.down(1))
         self.button2.pack(side='bottom',pady=0)
 
-    def up(self, args):
+    def up(self, args: int):
         for i in range(args):
             if (self.cur + 1) < len(self.val):
                 self.cur += 1
@@ -109,7 +109,7 @@ class CTkSelector(tk.CTkFrame):
 
         return self.string.get()
 
-    def down(self, args):
+    def down(self, args: int):
         for i in range(args):
             if (self.cur - 1) > -1:
                 self.cur -= 1
@@ -132,8 +132,12 @@ class CTkSelector(tk.CTkFrame):
         return self.string.get()
 
 class CTkMultiList(tk.CTkFrame):
-    def __init__(self, master, text='', values=[], command=None, width=200, height=150, **kwargs):
-        super().__init__(master=master, width=width, height=height)
+    def __init__(self, master, text='', values=[], command=lambda: None, width=200, height=150, fg_color=None, text_color='white', label_fg_color='transparent', checkbox_fg_color=None, checkbox_hover_color=None, checkbox_text_color='white', **kwargs):
+        super().__init__(master=master, width=width, height=height, fg_color=fg_color, **kwargs)
+
+        self.checkbox_fg_color = checkbox_fg_color
+        self.checkbox_hover_color = checkbox_hover_color
+        self.checkbox_text_color = checkbox_text_color
 
         self.pack_propagate(False)
 
@@ -141,18 +145,20 @@ class CTkMultiList(tk.CTkFrame):
         self.values = values
         self.command = command
 
-        self.label = tk.CTkLabel(self,text=text)
-        self.label.pack()
+        self.label = tk.CTkLabel(self, text=text, text_color=text_color, fg_color=label_fg_color)
+        self.label.pack(fill='x')
 
-        self.slideframe = tk.CTkScrollableFrame(self, fg_color=self._fg_color,scrollbar_fg_color='transparent',scrollbar_button_color=self._fg_color, scrollbar_button_hover_color=self._fg_color)
+        self.slideframe = tk.CTkScrollableFrame(self, fg_color=fg_color, scrollbar_fg_color='transparent', scrollbar_button_color=fg_color, scrollbar_button_hover_color=fg_color)
         self.slideframe.pack(pady=2)
 
         self.update()
 
     def update(self):
+        for i in self.slideframe.winfo_children():
+            i.destroy()
         for i in self.values:
             a = tk.CTkCheckBox(self.slideframe,text=i)
-            a.configure(command=lambda i=i, a=a: self.change(i, a.get()))
+            a = tk.CTkCheckBox(self.slideframe, text=i, fg_color=self.checkbox_fg_color, hover_color=self.checkbox_hover_color, text_color=self.checkbox_text_color)
             a.pack(pady=2)
 
     def get(self):
